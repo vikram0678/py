b629c95ab1e1f1b08995930e2c08cd3d1180f851
Vikram <vickyspace6@gmail.com>
2025-10-22 09:19:31 +0530

    Create numJewelsInStones.py

---
+++ numJewelsInStones.py
@@ -0,0 +1,9 @@
+def numJewelsInStones(jewels, stones):
+    jset = set(jewels)
+    count = 0
+    for s in stones:
+        if s in jset:
+            count += 1
+    return count
+jewels = "aA", stones = "aAAbbbb"
+print(numJewelsInStones(jewels, stones))  # Output: 3
