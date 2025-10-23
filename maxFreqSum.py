0c617087958788d65374d2ea72878a510db5bb72
Vikram <nandimandalamvikram6@gmail.com>
2025-10-23 07:22:15 +0530

    Update maxFreqSum.py

--- maxFreqSum.py
+++ maxFreqSum.py
@@ -30,3 +30,25 @@ class Solution:
 
 s="successes"
 print(maxFreqSum(s))
+
+
+
+
+# var maxFreqSum = function(s) {
+#     let map ={};
+#     for(let i = 0;i < s.length ; i++){
+#         map[s[i]] = !map[s[i]] ? 1 : ++map[s[i]];
+#     }
+#     let vowels = ['a', 'e', 'i', 'o', 'u']
+#     let maxVowels = 0;
+#     let maxConsonant = 0;
+#     let mapKeys = Object.keys(map);
+#     for(let i = 0; i< mapKeys.length; i++){
+#         if(vowels.includes(mapKeys[i])){
+#             maxVowels = Math.max(maxVowels, map[mapKeys[i]])
+#         }else{
+#             maxConsonant = Math.max(maxConsonant, map[mapKeys[i]])
+#         }
+#     }
+#     return maxVowels + maxConsonant
+# };
